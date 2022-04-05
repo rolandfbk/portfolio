10b303faaf75f9911c6e2c153b429bb5809a8e56
Roland <rolandfbk@gmail.com>
2022-04-05 18:39:32 +0200

    Project popup UI for desktop version done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //Menu implementation
-/* const showMenu = document.querySelector('.toogle-menu');
+const showMenu = document.querySelector('.toogle-menu');
 
 showMenu.addEventListener('click', () => {
   document.getElementById('wrapper').style.display = 'none';
@@ -38,62 +38,5 @@ const closeMenuContact = document.querySelector('.close-menu-contact');
 
 closeMenuContact.addEventListener('click', () => {
   document.getElementById('wrapper-menu').style.display = 'none';
-  document.getElementById('wrapper').style.display = 'flex';
-}); */
-
-//Project details implementation
-const project_details = {
-  title: 'Project name goes here',
-  description_first: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
-  description_second: 'Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-  image_slideshow: 'img/slideshow.svg',
-  languages: {
-    language_1: 'HTML/CSS',
-    language_2: 'Ruby on Rails',
-    language_3: 'JavaScript'
-  }
-}
-
-const project_details_title = document.getElementById('project_details_title');
-project_details_title.textContent = project_details.title;
-
-const project_details_language = document.getElementById('project_details_language');
-
-let list = [];
-let arr_lang = [];
-let count = 0;
-for (let lang in project_details.languages)
-{
-  list[count] = document.createElement('li');
-  list[count].className = 'courses';
-
-  arr_lang[count] = project_details.languages[lang];
-  list[count].textContent = arr_lang[count];
-  project_details_language.appendChild(list[count]);
-  count++;
-}
-
-const project_details_slideshow = document.getElementById('project_details_slideshow');
-project_details_slideshow.innerHTML = '<img src="'+project_details.image_slideshow+'" alt="">';
-
-const project_details_des_1 = document.getElementById('project_details_des_1');
-project_details_des_1.textContent = project_details.description_first;
-
-const project_details_des_2 = document.getElementById('project_details_des_2');
-project_details_des_2.textContent = project_details.description_second;
-
-const showProjectDetails = document.querySelectorAll('.project-button');
-
-for (const button of showProjectDetails) {
-  button.addEventListener('click', () => {
-    document.getElementById('wrapper').style.display = 'none';
-    document.getElementById('wrapper-project-popup').style.display = 'flex';
-  });
-}
-
-const closeProjectDetails = document.querySelector('.close-project-popup');
-
-closeProjectDetails.addEventListener('click', () => {
-  document.getElementById('wrapper-project-popup').style.display = 'none';
   document.getElementById('wrapper').style.display = 'flex';
 });
