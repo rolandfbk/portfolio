38f7f7f595931c6b4b69364506a3650fb239076e
afikanzoro <muchemimutahi@gmail.com>
2022-04-06 21:10:29 +0300

    Add code to the validation.js file

--- js/validation.js
+++ js/validation.js
@@ -2,3 +2,15 @@ const contactForm = document.getElementById('user-form');
 const errorMessage = document.getElementById('error_message');
 const errMsg = 'Message not sent! Please write your email in small letters';
 
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+      let email = document.getElementById('email').value;
+      email.trim();
+    if (email != email.toLowerCase()) {
+    errorMessage.textContent = errMsg;
+    }else {
+        contactForm.submit();
+      }
+    });
+    
+
