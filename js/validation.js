a3e22a4c5227540fb9579979c7f70ba5f1b30f8c
Roland <rolandfbk@gmail.com>
2022-04-06 17:59:55 +0200

    Add part of the email validation

---
+++ js/validation.js
@@ -0,0 +1,4 @@
+const contactForm = document.getElementById('user-form');
+const errorMessage = document.getElementById('error_message');
+const errMsg = 'Message not sent! Please write your email in small letters';
+
