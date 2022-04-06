53391aa5e65321880eb2ab06927fe0cb7ee2606a
Roland <rolandfbk@gmail.com>
2022-04-06 21:02:47 +0200

    Js linters fixed

--- js/validation.js
+++ js/validation.js
@@ -2,15 +2,13 @@ const contactForm = document.getElementById('user-form');
 const errorMessage = document.getElementById('error_message');
 const errMsg = 'Message not sent! Please write your email in small letters';
 
-contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-      let email = document.getElementById('email').value;
-      email.trim();
-    if (email != email.toLowerCase()) {
+contactForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  email.trim();
+  if (email !== email.toLowerCase()) {
     errorMessage.textContent = errMsg;
-    }else {
-        contactForm.submit();
-      }
-    });
-    
-
+  } else {
+    contactForm.submit();
+  }
+});
